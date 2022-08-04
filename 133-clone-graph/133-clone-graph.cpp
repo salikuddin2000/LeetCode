d432605ce358cc028edb4d6803357d8ebf24cc50
Salik Uddin <58416002+salikuddin2000@users.noreply.github.com>
2022-08-04 16:39:41 +0530

    Time: 14 ms (24.72%), Space: 28.2 MB (12.57%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    vector<Node*> vis= std :: vector<Node*> (1e5);
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node) return node;
+        if(vis[node->val]!=NULL) return vis[node->val];
+        Node* root= new Node(node->val);
+        if(node->neighbors.empty()) return root;
+        vis[node->val]=root;
+        for(auto it: node->neighbors){
+            root->neighbors.push_back(cloneGraph(it));
+        }
+        return root;
+    }
+};

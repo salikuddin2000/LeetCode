3eb1d58813392d9fb992ecb130af56264430a974
Salik Uddin <58416002+salikuddin2000@users.noreply.github.com>
2022-08-04 17:16:39 +0530

    Time: 7 ms (80.17%), Space: 8.8 MB (66.05%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -20,7 +20,7 @@ public:
 */
 
 class Solution {
-    vector<Node*> vis= std :: vector<Node*> (120);
+    vector<Node*> vis= std :: vector<Node*> (200);
 public:
     Node* cloneGraph(Node* node) {
         if(!node) return node;

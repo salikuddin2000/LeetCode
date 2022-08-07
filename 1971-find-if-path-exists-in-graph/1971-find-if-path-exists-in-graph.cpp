e4c968644814fa92c2e8c971112369fad71c94f0
Salik Uddin <58416002+salikuddin2000@users.noreply.github.com>
2022-08-07 18:10:32 +0530

    Time: 624 ms (94.96%), Space: 112.5 MB (96.22%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,15 +1,47 @@
 class Solution {
-    vector<int> parent;
 public:
-    int find_set(int v){
-        if(v==parent[v]) return v;
-        return parent[v]=find_set(parent[v]);
-    }
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        for(int i=0;i<n;i++) parent.push_back(i);
-        for(auto itr:edges){
-            parent[find_set(itr[0])]=find_set(itr[1]);
+    vector<int> parent;
+    vector<int> sz;
+    
+    void make_set(int n) {
+        for(int i=0;i<n;i++) {
+            parent[i]=i;
+            sz[i]=1;
         }
-        return find_set(source)==find_set(destination);
+    }
+    
+    int find_set(int a) {
+      if(parent[a]==a)
+          return a;
+      return parent[a]=find_set(parent[a]);
+    }
+    void union_set(int a, int b) {
+        a=find_set(a);
+        b= find_set(b);
+        if(a!=b) {
+            if(sz[a]<sz[b]) 
+              swap(a,b);
+            parent[b]=a;
+            sz[a]+=sz[b];
+        }
+    }
+    
+    
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+      parent.assign(n,-1);
+      sz.assign(n,0);
+      make_set(n);
+      for(int i=0;i<edges.size();i++) {
+           int u = edges[i][0];
+           int v = edges[i][1];
+          int a = find_set(u);
+          int b = find_set(v);
+          if(a!=b) {
+               union_set(u,v);
+          }
+      }
+      int a = find_set(source);
+      int b = find_set(destination);
+      return a==b;
     }
 };

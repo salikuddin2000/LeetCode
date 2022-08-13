097cf99da2e78b19d14a0fa3538864fddcc9786b
Salik Uddin <58416002+salikuddin2000@users.noreply.github.com>
2022-08-13 19:47:04 +0530

    Time: 199 ms (54.40%), Space: 105.1 MB (5.27%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+
+public:
+    int sell(vector<int>& prices,int pos,int n,vector<int> &minPrice){
+        if(pos==n-1) return prices[n-1];
+        if(minPrice[pos]!=-1) return minPrice[pos];
+        else minPrice[pos]= max(prices[pos],sell(prices,pos+1,n,minPrice));
+        return minPrice[pos];
+    }
+    int buy(vector<int>& prices,int pos,int n,vector<int> &minPrice){
+        if(pos==n-1) return 0;
+        return max((sell(prices,pos+1,n,minPrice)-prices[pos]),buy(prices,pos+1,n,minPrice));
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();  
+        vector<int>minPrice=std:: vector<int>(n+1,-1);
+        return buy(prices,0,n,minPrice);
+    }
+};
